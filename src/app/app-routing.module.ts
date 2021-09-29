41ce446e3afd47452a5a3cd4efb6f11b542e31b1
Wayne <waynefed2107@gmail.com>
2021-09-28 20:23:38 -0400

    created initial router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { SplashComponent } from './splash/splash.component';
+
 const routes: Routes = [
-  
+  {
+    path: 'splash',
+    component: SplashComponent
+  },
+  {
+    path: '',
+    redirectTo: '/splash',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
