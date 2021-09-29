31a39bef3a6cafd4987087531cc259a4d967f16b
Wayne <waynefed2107@gmail.com>
2021-09-28 21:00:31 -0400

    created login form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashComponent } from './splash/splash.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SplashComponent
+    SplashComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
