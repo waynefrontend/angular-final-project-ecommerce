0f2f5590cc16fe27aa14776910543c333bc8e866
Wayne <waynefed2107@gmail.com>
2021-09-28 20:12:33 -0400

    generating splash component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { SplashComponent } from './splash/splash.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
