3106351ade18cafcfab489a15166afe7a2eb7952
Wayne <waynefed2107@gmail.com>
2021-09-30 20:46:36 -0400

    tested that child paths are working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowseComponent } from './browse/browse.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 import { NewUserComponent } from './new-user/new-user.component';
 
 import { SplashComponent } from './splash/splash.component';
